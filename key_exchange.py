7a522f2fa6e26adb413b63bcf7914acb21c52615
Duje Nikolić <dujerancic@gmail.com>
2024-05-06 20:25:09 +0200

    Add Diffie-Hellman functions

--- key_exchange.py
+++ key_exchange.py
@@ -1,5 +1,8 @@
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import dh
+from cryptography.hazmat.primitives.kdf.hkdf import HKDF
 
 def generate_RSA_private_key():
     RSA_private_key = rsa.generate_private_key(
@@ -36,3 +39,34 @@ def generate_RSA_public_key(RSA_private_key):
     print(f"RSA public key:\n {RSA_public_key_decoded}")
 
     return RSA_public_key
+
+def generate_DH_parameters():
+    DH_parameters = dh.generate_parameters(generator=2, key_size=2048)
+
+    g = DH_parameters.parameter_numbers().g
+    p = DH_parameters.parameter_numbers().p
+
+    print(f"Diffie-Hellman parameters:\ng -> {g}\np -> {p}")
+
+    return DH_parameters
+
+def generate_DH_private_key(DH_parameters):
+    DH_private_key = DH_parameters.generate_private_key()
+
+    print(f"Diffie-Hellman private key:\n{DH_private_key}")
+
+    return DH_private_key
+
+def generate_DH_public_key(DH_private_key):
+    DH_public_key = DH_private_key.public_key()
+
+    print(f"Diffie-Hellman public key:\n{DH_public_key}")
+
+    return DH_public_key
+
+def get_DH_shared_key(DH_private_key, DH_public_key):
+    DH_shared_key = DH_private_key.exchange(DH_public_key)
+
+    print(f"Diffie-Hellman shared key:\n{DH_shared_key}")
+
+    return DH_shared_key
